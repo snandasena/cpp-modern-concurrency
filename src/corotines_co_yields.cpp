bc8debea274da0cf98f85471513e1857b80a23c0
snandasena <sajithgithub@gmail.com>
2023-12-10 11:13:19 +0100

    added co_yield sample

---
+++ src/corotines_co_yields.cpp
@@ -0,0 +1,135 @@
+//
+// Created by sajit on 05/12/2023.
+//
+
+#include <coroutine>
+#include <iostream>
+#include <stdexcept>
+#include <thread>
+
+template<typename T>
+struct Generator
+{
+    struct promise_type;
+    using handle_type = std::coroutine_handle<promise_type>;
+
+    struct promise_type
+    {
+        T value_;
+        std::exception_ptr exception_;
+
+        Generator get_return_object()
+        {
+            return Generator(handle_type::from_promise(*this));
+        }
+
+        std::suspend_always initial_suspend() { return {}; }
+
+        std::suspend_always final_suspend() noexcept { return {}; }
+
+        void unhandled_exception() { exception_ = std::current_exception(); }
+
+        template<std::convertible_to<T> From>
+        std::suspend_always yield_value(From &&from)
+        {
+            value_ = std::forward<From>(from);
+            return {};
+        }
+
+        void return_void() {}
+    };
+
+    handle_type h_;
+
+    Generator(handle_type h) : h_(h) {}
+
+    ~Generator()
+    {
+        h_.destroy();
+    }
+
+    explicit operator bool()
+    {
+        fill();
+        return !h_.done();
+    }
+
+    T operator()()
+    {
+        fill();
+        full_ = false;
+
+        return std::move(h_.promise().value_);
+    }
+
+private:
+
+    bool full_ = false;
+
+    void fill()
+    {
+        if (!full_)
+        {
+            h_();
+            if (h_.promise().exception_)
+                std::rethrow_exception(h_.promise().exception_);
+
+            full_ = true;
+        }
+    }
+};
+
+
+Generator<std::uint64_t>
+fibonacci_sequence(unsigned n)
+{
+    if (n == 0)
+        co_return;
+
+    if (n > 94)
+        throw std::runtime_error{"Too big Fibonacci sequence"};
+
+    co_yield 0;
+
+    if (n == 1)
+        co_return;
+
+    co_yield 1;
+    if (n == 2)
+        co_return;
+
+    std::uint64_t a = 0;
+    std::uint64_t b = 1;
+
+    for (unsigned i = 2; i < n; ++i)
+    {
+        auto s = a + b;
+        co_yield s;
+
+        a = b;
+        b = s;
+    }
+}
+
+int main()
+{
+    try
+    {
+        auto gen = fibonacci_sequence(10);
+
+        for (int i = 0; gen; ++i)
+        {
+            std::cout << "fib(" << i << ")" << gen() << '\n';
+        }
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Exception :" << e.what() << '\n';
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown exception\n";
+    }
+
+    return 0;
+}
